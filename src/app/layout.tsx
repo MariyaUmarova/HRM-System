e1dc539cec068825307a52a727bd15fd8e1b034f
uziuzborn <m.a.o.umarov@gmail.com>
2026-09-03 23:18:50 +0300

    Load Recruit reference parity overrides

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./recruit.css";
+import "./recruit-reference-parity.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
